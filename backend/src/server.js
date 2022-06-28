43d14a17eef0deaf430c6527a24e6c3511103eb9
João Pedro Correia Barros <joao.barros@opah.com.br>
2022-06-28 14:07:44 -0300

    backend package json mistake

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,9 @@ const routes = require('./routes');
 require('./database');
 
 const app = express();
+const port = 3333;
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(port, () => console.log(`Server is running on port ${port} 🤖`));
---
+++ frontend/src/AppRoutes.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+import LoginPage from './pages/LoginPage';
+// import HomePage from './pages/HomePage';
+
+import { AuthContext } from './contexts/auth';
+
+const AppRoutes = () => {
+  const [user, setUser] = useState(null);
+
+  const login = (email, password) => {
+    console.log('login', {email, password});
+    setUser({id: "123", email})
+  };
+
+  const logout = () => {
+    console.log('logout');
+  };
+
+  return (
+    <Router>
+      <AuthContext.Provider value={{ authenticated: !!user, user, login }}>
+        <Routes>
+          <Route exact path="/login" element={<LoginPage />} />
+          {/* <Route exact path="/" element={<HomePage />} /> */}
+        </Routes>
+      </AuthContext.Provider>
+    </Router>
+  );
+};
+
+export default AppRoutes;
